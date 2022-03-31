bcf8b6d1c5c7f7a1c850cff5d9e7061d58b41613
abinea <1655853551@qq.com>
2022-03-31 20:07:31 +0800

    feat: images lazyload

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import lazyPlugin from "vue3-lazy";
 
 // 全局引入样式文件
 import "@/assets/scss/index.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(router).use(store);
+
+app.use(lazyPlugin, {
+  loading: require("@/assets/images/default.png"),
+});
+
+app.mount("#app");
