6e2abd4d2cf7af18b8f461373bf1cd405639ff37
abinea <1655853551@qq.com>
2022-04-09 13:46:41 +0800

    feat: toplist

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Toplist from "@/views/top-list"
 import Search from "@/views/search"
 import SingerDetail from "@/views/singer-detail"
 import Album from "@/views/album"
+import TopDetail from "@/views/top-detail"
 
 const routes = [
   {
@@ -34,6 +35,12 @@ const routes = [
   {
     path: "/top-list",
     component: Toplist,
+    children: [
+      {
+        path: ":id",
+        component: TopDetail,
+      },
+    ],
   },
   {
     path: "/search",
