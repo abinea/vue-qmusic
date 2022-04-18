c3da7bf8a06928c348b2ae0545d626ff6f03f97f
abinea <1655853551@qq.com>
2022-04-18 22:25:11 +0800

    delete:change default cover

--- src/components/base/no-result/no-result.vue
+++ src/components/base/no-result/no-result.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="no-result">
     <div class="no-result-content">
-      <div class="icon"></div>
       <p class="text">{{ title }}</p>
     </div>
   </div>
@@ -31,13 +30,6 @@ export default {
   transform: translate3d(-50%, -50%, 0);
   .no-result-content {
     text-align: center;
-    .icon {
-      width: 86px;
-      height: 90px;
-      margin: 0 auto;
-      @include bg-image("no-result");
-      background-size: 86px 90px;
-    }
     .text {
       margin-top: 30px;
       font-size: $font-size-medium;
